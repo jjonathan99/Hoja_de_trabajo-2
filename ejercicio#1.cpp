f72a0b5321838735c88579497967ab3b93165b85
Jonathan paredes <jparedes1919gmail.com>
2020-04-19 12:04:25 -0600

    Agregamos comentario

--- ejercicio#1.cpp
+++ ejercicio#1.cpp
@@ -1,4 +1,4 @@
-/*Ejercicio # 1:
+/*Ejercicio # 1: prueba git
 Se dispone de un archivo STOCK correspondiente a la existencia de artículos de un almacén y se desea señalar aquellos artículos cuyo nivel está por debajo del mínimo y que visualicen un mensaje “hacer pedido”. Cada artículo contiene un registro con los siguientes campos: número del código del artículo, nivel mínimo, nivel actual, proveedor, precio.
 
 El programa debe tener un menu que se especifiquen las siguientes opciones:
